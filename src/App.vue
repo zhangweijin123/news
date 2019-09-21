b0ba66f93ab739373c38007bac779ed5e08af1aa
zhangweijin123 <615745978@qq.com>
2019-09-21 19:51:27 +0800

    “firstcommit”

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="colorRed">
+      这是第一个单文件组件，{{message}}, 132465456
+
+      <router-link to="/login">登录页</router-link>
+      
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: "hello vue"
+        }
+    }
+} 
+</script>
+
+<style>
+    .colorRed{
+        color:red;
+    }
+</style>
