861a879019209b7c694eb73d3892b483620260a6
zhangweijin123 <615745978@qq.com>
2019-09-22 16:14:55 +0800

    添加公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      
+      <!-- 组件占位符 -->
       <router-view></router-view>
   </div>
 </template>
@@ -16,4 +16,20 @@ export default {
 </script>
 
 <style>
+*{
+    margin:0;
+    padding:0
+}
+a{
+    color:inherit;
+}
+a:hover{
+    text-decoration: underline;
+}
+ul,li,ol{
+    list-style: none;
+}
+i,em{
+    font-style: none;
+}
 </style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,7 +4,11 @@
 
 <script>
 export default {
-
+    data(){
+      return{
+        list:[]
+      }
+    }
 }
 </script>
 
